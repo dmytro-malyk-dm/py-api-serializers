6f06c9e8d62f55687e8307439d76a6fdfab87735
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-12-30 16:31:15 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -27,16 +27,20 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.first_name + " " + self.last_name
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
-    genres = models.ManyToManyField(Genre)
-    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre, related_name="movies")
+    actors = models.ManyToManyField(Actor, related_name="movies")
 
     class Meta:
         ordering = ["title"]
@@ -47,20 +51,30 @@ class Movie(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
+    )
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
+    )
 
     class Meta:
         ordering = ["-show_time"]
 
-    def __str__(self):
-        return self.movie.title + " " + str(self.show_time)
+    def __str__(self) -> str:
+        return f"{self.movie.title} {str(self.show_time)}"
 
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
     )
 
     def __str__(self):
@@ -82,8 +96,8 @@ class Ticket(models.Model):
 
     def clean(self):
         for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-            (self.row, "row", "count_rows"),
-            (self.seat, "seat", "count_seats_in_row"),
+            (self.row, "row", "rows"),
+            (self.seat, "seat", "seats_in_row"),
         ]:
             count_attrs = getattr(
                 self.movie_session.cinema_hall, cinema_hall_attr_name
@@ -97,6 +111,10 @@ class Ticket(models.Model):
                         f"(1, {count_attrs})"
                     }
                 )
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return (
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,35 @@
-# write urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import (
+    ActorViewSet,
+    GenreViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+)
+
+router = DefaultRouter()
+router.register("actors", ActorViewSet, basename="actor")
+router.register("genres", GenreViewSet, basename="genre")
+router.register(
+    "cinema_halls",
+    CinemaHallViewSet,
+    basename="cinemahall"
+)
+router.register(
+    "movies",
+    MovieViewSet,
+    basename="movie"
+)
+router.register(
+    "movie_sessions",
+    MovieSessionViewSet,
+    basename="moviesession"
+)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "cinema"
